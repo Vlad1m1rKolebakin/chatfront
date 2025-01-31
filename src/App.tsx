fc78480c036b26a1eed18159863f7c38879874ab
Vlad1m1rKolebakin <kolebakin.v@yandex.ru>
2025-01-31 10:59:32 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:5000'); // Подключаемся к серверу
+
+function App() {
+  const [messages, setMessages] = useState<string[]>([]);
+  const [inputValue, setInputValue] = useState('');
+
+  useEffect(() => {
+    // Получение сообщений от сервера
+    socket.on('chat-message', (data: string) => {
+      setMessages((prevMessages) => [...prevMessages, data]);
+    });
+  }, []);
+
+  
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (inputValue.trim()) {
+      socket.emit('chat-message', inputValue); // Отправляем сообщение на сервер
+      setInputValue('');
+    }
+  };
+
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Chat Application</h1>
+      <ul>
+        {messages.map((message, index) => (
+          <li key={index}>{message}</li>
+        ))}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={inputValue} onChange={handleChange} />
+        <button type="submit">Отправить</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
